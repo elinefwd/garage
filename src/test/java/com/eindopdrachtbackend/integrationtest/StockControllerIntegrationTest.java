88b53f6a1371b2b712e133ed7aba4acce90baccf
elinefwd <elinefwd@gmail.com>
2024-10-18 10:29:56 +0200

    lasttings

--- src/test/java/com/eindopdrachtbackend/integrationtest/StockControllerIntegrationTest.java
+++ src/test/java/com/eindopdrachtbackend/integrationtest/StockControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test; // For @Test annotation
 
 import com.eindopdrachtbackend.model.Stock;
 import com.eindopdrachtbackend.service.StockService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
